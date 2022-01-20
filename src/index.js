64b5b9bd6e4f3263d9d20c43be4e60a3168092d9
Nelson Rosales <56582884+nelson2411@users.noreply.github.com>
2022-01-20 21:55:02 +0100

    GraphQL for cart implemented

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-
+import { resolvers, typeDefs } from "./graphql/resolvers";
 import { store, persistor } from "./redux/store";
 import { ApolloProvider } from "react-apollo";
 import { createHttpLink } from "apollo-link-http";
@@ -19,6 +19,17 @@ const cache = new InMemoryCache();
 const client = new ApolloClient({
   link: httpLink,
   cache,
+  typeDefs,
+  resolvers,
+});
+
+client.writeData({
+  data: {
+    cartHidden: true,
+    cartItems: [],
+    itemCount: 0,
+    cartTotal: 0,
+  },
 });
 
 client
